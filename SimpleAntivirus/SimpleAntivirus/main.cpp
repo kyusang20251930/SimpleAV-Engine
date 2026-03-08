eecd215a2c09b6dbe08494e7b5ae6a222b25ed63
kyusang <kyusang20251930@gmail.com>
2026-03-08 23:38:14 +0900

    feat: Wincrypt API를 활용한 파일 MD5 해시 추출 기능 구현

--- SimpleAntivirus/SimpleAntivirus/main.cpp
+++ SimpleAntivirus/SimpleAntivirus/main.cpp
@@ -1,5 +1,39 @@
 #include<windows.h> //윈도우 운영체제의 기능을 쓰기 위한 윈도우 전용 도구 상자
 #include<iostream> //C++ 의 기본 도구 상자, 화면 글자를 출력하거나 입력을 받을 때 사용
+#include<wincrypt.h> //암호 도구
+#include<iostream>
+#include<iomanip> //16진수 출력을 위해 필요
+
+void PrintMD5(BYTE* payload, DWORD payloadSize) {
+	HCRYPTPROV hProv = 0;
+	HCRYPTHASH hHash = 0;
+	BYTE rgbHash[16]; //MD5는 항상 16바이트(128비트)이다.
+	DWORD cbHash = 16;
+
+	//암호 서비스 제공자(CSP) 연결
+	if (CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
+
+		//MD5 해시 객체 생성
+		if (CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash)) {
+
+			//데이터 주입
+			if (CryptHashData(hHash, payload, payloadSize, 0)) {
+
+				//결과값 가져오기
+				if (CryptGetHashParam(hHash, HP_HASHVAL, rgbHash, &cbHash, 0)) {
+					std::cout << "MD5 Hash: ";
+					for (DWORD i=0; i < cbHash; i++) {
+						//16진수로 출력
+						std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)rgbHash[i];
+					}
+					std::cout << std::dec << std::endl;
+				}
+			}
+			CryptDestroyHash(hHash);
+		}
+		CryptReleaseContext(hProv, 0);
+	}
+}
 
 int main(void) {
 	//C++ 에는 int, char 같은 기본 자료형이 있지만, windows.h를 가져오게 되면
@@ -36,6 +70,7 @@ int main(void) {
 	char* fileBuffer = new char[fileSize];
 
 	if (ReadFile(hFile, fileBuffer, fileSize, &bytesRead, NULL)) {
+
 		std::cout << "파일 전체를 메모리에 로드 했습니다. (" << bytesRead << " bytes)" << std::endl;
 		//이제 fileBuffer[0] 부터 fileBufer[fileSize-1] 까지 모든 데이터를 검사 할 수 있다.
 
@@ -46,6 +81,8 @@ int main(void) {
 		else {
 			std::cout << "경고 : 실행 파일(MZ) 형식이 아닙니다." << std::endl;
 		}
+
+		PrintMD5((BYTE*)fileBuffer, bytesRead);
 	}
 
 	//다 사용했으면 반납
